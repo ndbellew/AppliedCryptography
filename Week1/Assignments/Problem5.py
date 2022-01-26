e041ea1e13391a0df4418f86a35e8bd0ce1c5c36
Nathan <ndbellew@gmail.com>
2022-01-26 08:29:46 -0700

    Went to class now theres more stuff

--- Week1/Assignments/Problem5.py
+++ Week1/Assignments/Problem5.py
@@ -1,5 +1,6 @@
 # HW 1
 import string
+import enchant
 AlphaLower = string.ascii_lowercase
 AlphaUpper = string.ascii_uppercase
 
@@ -7,11 +8,12 @@ def readFile(File):
     with open(File) as f:
         read_data = f.read()
     f.close()
-    return read_data
+    return read_data.split("\r\n")# Returns a list separated by the endline parameter for strings
 
-def writeFile(File, data):
-    with open(File) as f:
-        f.write(data)
+def writeFile(File, Text):
+    with open(File, 'w') as f:
+        for sentence in Text:
+            f.write(sentence+"\n")
     f.close()
         
 def main():
@@ -19,25 +21,52 @@ def main():
     inputC = readFile("class_input_c.txt")
     EnKey = int(input("Enter an Encryption Key\n"))%26
     outB = Encrypt(inputB, EnKey)
+    outC = Decrypt(inputC)
     writeFile("class_output_b.txt", outB)
+    if outC is int():
+        print(outC)
+    writeFile("class_output_c.txt", outC)
 
 def Encrypt(Text, Key):
-    NewText = ""
-    for letter in Text:
-        if letter in AlphaLower:
-            index = AlphaLower.index(letter)
-            print(index)
-            NewText += AlphaLower[((index+Key)%len(AlphaLower))]
-        elif letter in AlphaUpper:
-            index = AlphaUpper.index(letter)
-            NewText += AlphaUpper[((index+Key)%len(AlphaUpper))]
-        else:
-            # Punctuation or spaces
-            NewText += letter
+    NewText = []
+    NewSentence = ""
+    for sentence in Text:
+        for letter in sentence:
+            if letter in AlphaLower:
+                index = AlphaLower.index(letter)
+                NewSentence += AlphaLower[((index+Key)%len(AlphaLower))]
+            elif letter in AlphaUpper:
+                index = AlphaUpper.index(letter)
+                NewSentence += AlphaUpper[((index+Key)%len(AlphaUpper))]
+            else:
+                # Punctuation or spaces
+                NewSentence += letter
+        NewText.append(NewSentence)
     return NewText
 
-def Decrypt(Text, Key):
-    pass
+def Decrypt(Text):
+    for Key in range(26):
+        NewText = []
+        NewSentence = ""
+        for sentence in Text:
+            for letter in sentence:
+                if letter in AlphaLower:
+                    index = AlphaLower.index(letter)
+                    NewSentence += AlphaLower[((index+Key)%len(AlphaLower))]
+                elif letter in AlphaUpper:
+                    index = AlphaUpper.index(letter)
+                    NewSentence += AlphaUpper[((index+Key)%len(AlphaUpper))]
+                else:
+                    # Punctuation or spaces
+                    NewSentence += letter
+            NewText.append(NewSentence)
+        Test = NewText[0].split()[0] # This will take a sentence and split it down into a single word
+                                    # (in this instance its justa  word but will still work)
+        print(Test)
+        EnglishDictionary = enchant.Dict("en_US")
+        if EnglishDictionary.check(Test):
+            return NewText
+    return 0 # Should throw error
 
 if __name__ == "__main__":
     main()
---
+++ Week2/ClassNotes.md
@@ -0,0 +1,25 @@
+# Class Knotes
+
+## Security vs Utility/Practicality
+
+### Secure vs. Insecure can be a spectrum. 
+
+1. A ssytem secure in every objections and purposes with zero vulnerabilities has no utility. The only truly secure system is one that isn't plugged in. 
+2. We use systems which are imperfect in security and has known vulnerabilties and threats. A lot of things require people to be proactive and think about possible issues prior to them happening.
+   - Example: Internet (Slide 23)
+3. Assessing risk and balancing the tradeoff is important for example OTP MFA (Multi-Factor Authentication)
+
+## Crypto Model 
+
+### Some people use different info to relate to different topics
+
+Alice -> Bob (legitimate)
+Eve (Passive Eavesdropping)
+Mallory (Active)
+
+For alice and bob one has the information and the other does not, this is a common computer network context or data purposes. 
+
+```mermaid
+graph LR
+    fa:fa-check-->fa:fa-coffee
+```
